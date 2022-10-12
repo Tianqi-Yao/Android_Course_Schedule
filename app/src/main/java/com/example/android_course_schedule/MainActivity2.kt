31bac750f23caf9e5bf1ab3c904a47fb616c4925
Tianqi <yaotianqi6666@qq.com>
2022-10-12 02:16:30 -0500

    实现表格功能

--- app/src/main/java/com/example/android_course_schedule/MainActivity2.kt
+++ app/src/main/java/com/example/android_course_schedule/MainActivity2.kt
@@ -1,7 +1,6 @@
 package com.example.android_course_schedule
 
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,10 +8,9 @@ import android.view.View
 
 import android.widget.Button
 import android.widget.Toast
-import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,34 +29,82 @@ class MainActivity2 : AppCompatActivity() {
 
         // recyclerView
         val itemsList = ArrayList<Items>()
-
         for (i in 0..100) {
             val items = Items("name$i", "FL${2000+i}")
             itemsList.add(items)
         }
         Log.d("test", itemsList.toString())
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recycleView)
+        // left data
+        val leftItemsList = ArrayList<Items>()
+        for (i in 6..20) {
+            val items = Items("$i o'clock", "")
+            leftItemsList.add(items)
+        }
+
+        // top data
+        val topItemsList = ArrayList<Items>()
+        for (i in 1..7) {
+            val items = Items("day $i", "")
+            topItemsList.add(items)
+        }
+
+        initRecyclerView(itemsList,R.id.recycleView,true,GridLayoutManager(applicationContext,7))
+
+        //left
+        initRecyclerView(leftItemsList,R.id.left_recyclerView,false,GridLayoutManager(applicationContext,1))
+        //top
+        initRecyclerView(topItemsList,R.id.top_recyclerView,false,GridLayoutManager(applicationContext,7))
+
+        // sync roll
+        val scrollView0 = findViewById<SyncHorizontalScrollView>(R.id.top_scroll)
+        val scrollView1 = findViewById<SyncHorizontalScrollView>(R.id.main_scroll)
+        scrollView0.setScrollView(scrollView1)
+        scrollView1.setScrollView(scrollView0)
+
+
+//        val recyclerView = findViewById<RecyclerView>(R.id.recycleView)
+//
+//        val adapter = RecyclerViewAdapter(itemsList)
+//        val layoutManager = GridLayoutManager(applicationContext,7)
+
+//        recyclerView.adapter = adapter
+//        recyclerView.layoutManager = layoutManager
+////        recyclerView.itemAnimator = DefaultItemAnimator()
+//        recyclerView.setHasFixedSize(true)
+//
+//        //click fun
+//        adapter.setOnItemClickListener(object : RecyclerViewAdapter.ClickListener<Items> {
+//            override fun onClick(view: View?, data: Items, position: Int) {
+//                Toast.makeText(applicationContext, "Position = $position  data = ${data.courseName}", Toast.LENGTH_LONG).show()
+//            }
+//        })
+
+    }
+
+    private fun initRecyclerView(itemsList : ArrayList<Items>, viewID : Int, allowedClick: Boolean, layoutManager: RecyclerView.LayoutManager){
+        val recyclerView = findViewById<RecyclerView>(viewID)
 
         val adapter = RecyclerViewAdapter(itemsList)
-        val layoutManager = GridLayoutManager(applicationContext,7)
+//        val layoutManager = GridLayoutManager(applicationContext,7)
 
         recyclerView.adapter = adapter
         recyclerView.layoutManager = layoutManager
-//        recyclerView.itemAnimator = DefaultItemAnimator()
         recyclerView.setHasFixedSize(true)
 
         //click fun
+
         adapter.setOnItemClickListener(object : RecyclerViewAdapter.ClickListener<Items> {
             override fun onClick(view: View?, data: Items, position: Int) {
-                Toast.makeText(applicationContext, "Position = $position  data = ${data.courseName}", Toast.LENGTH_LONG).show()
+                if (allowedClick) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Position = $position  data = ${data.courseName}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
             }
         })
 
     }
-
-//    private fun jumpFun() {
-//        val intent = Intent(this, MainActivity::class.java)
-//        startActivity(intent)
-//    }
 }
