e33f9fbc5f0850ee5e92b41dd6db5cae1526f87a
tianqi <yaotianqi6666@gmail.com>
2022-10-26 03:18:05 -0500

    update database

--- app/src/main/java/com/example/android_course_schedule/MainActivity2.kt
+++ app/src/main/java/com/example/android_course_schedule/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.android_course_schedule
 
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,16 +9,20 @@ import android.util.Log
 import android.view.View
 
 import android.widget.Button
-import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-
         val bt = findViewById<Button>(R.id.btn_back)
 
         bt.setOnClickListener {
@@ -25,12 +30,7 @@ class MainActivity2 : AppCompatActivity() {
         }
 
         // recyclerView
-        val itemsList = ArrayList<Items>()
-        for (i in 0..104) {
-            val items = Items("", "")
-            itemsList.add(items)
-        }
-        Log.d("test", itemsList.toString())
+        loadData()
 
         // left data
         val leftItemsList = ArrayList<Items>()
@@ -46,7 +46,7 @@ class MainActivity2 : AppCompatActivity() {
             topItemsList.add(items)
         }
 
-        initRecyclerView(itemsList,R.id.recycleView,true,GridLayoutManager(applicationContext,7))
+//        initRecyclerView(itemsList,R.id.recycleView,true,GridLayoutManager(applicationContext,7))
 
         //left
         initRecyclerView(leftItemsList,R.id.left_recyclerView,false,GridLayoutManager(applicationContext,1))
@@ -62,6 +62,7 @@ class MainActivity2 : AppCompatActivity() {
     }
 
     private fun initRecyclerView(itemsList : ArrayList<Items>, viewID : Int, allowedClick: Boolean, layoutManager: RecyclerView.LayoutManager){
+        Log.d("test","id: $viewID")
         val recyclerView = findViewById<RecyclerView>(viewID)
 
         val adapter = RecyclerViewAdapter(itemsList)
@@ -76,18 +77,65 @@ class MainActivity2 : AppCompatActivity() {
         adapter.setOnItemClickListener(object : RecyclerViewAdapter.ClickListener<Items> {
             override fun onClick(view: View?, data: Items, position: Int) {
                 if (allowedClick) {
-                    jumpFun()
+                    Log.d("test","position: $position")
+                    jumpFun(position)
                 }
             }
         })
 
     }
 
-    private fun jumpFun() {
+    private fun jumpFun(position: Int) {
         val intent = Intent(this, newClass::class.java)
-        val msg = "message"
-        intent.putExtra("msg",msg)
+        intent.putExtra("position", position)
         startActivity(intent)
 
     }
+
+    private fun loadData() {
+        // Read from the database
+        val database = Firebase.database
+        val myRef = database.reference.child("position")
+        Log.d("test","myRef: $myRef")
+        myRef.addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<Map<String, Any>>()
+                if (value != null) {
+                    val itemsList = ArrayList<Items>()
+                    loadMapData(value,itemsList)
+                    initRecyclerView(itemsList,R.id.recycleView,true,GridLayoutManager(applicationContext,7))
+                }
+                else{
+                    Log.d("test","do not have data from position")
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("test", "Failed to read value.", error.toException())
+            }
+
+        })
+    }
+
+
+    private fun loadMapData(value: Map<String, Any>,itemsList : ArrayList<Items>) {
+
+        for (i in 0..104) {
+            Log.d("test", "map value is: $i  ${value["$i"]}")
+            val courseInfo: Any? = value["$i"]
+            if (courseInfo is Map<*, *>) {
+                val courseName: String = courseInfo["courseName"] as String
+                val location: String = courseInfo["location"] as String
+                val item = Items(courseName, location)
+                itemsList.add(item)
+                Log.d("test", "loadMapData1111: $item ${itemsList[i]}")
+            } else {
+                val item = Items("", "")
+                itemsList.add(item)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/android_course_schedule/newClass.kt
+++ app/src/main/java/com/example/android_course_schedule/newClass.kt
@@ -4,18 +4,58 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class newClass : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_class)
 
-        val bt = findViewById<Button>(R.id.btn_back_newclass)
+        val btnBack = findViewById<Button>(R.id.btn_back_newclass)
+        val btnSubmit = findViewById<Button>(R.id.btn_submit)
 
-        bt.setOnClickListener {
+        val position  = intent.getIntExtra("position",-1)
+
+        val usernameET = findViewById<EditText>(R.id.et_coursename)
+        val locationET = findViewById<EditText>(R.id.et_location)
+
+        btnSubmit.setOnClickListener {
+            val username = usernameET.text.toString()
+            val location = locationET.text.toString()
+            writeData(position, username, location)
+            finish()
+        }
+
+        btnBack.setOnClickListener {
             finish()
         }
     }
 
+
+    private fun writeData(position:Int, courseName: String, location: String){
+        Log.d("test","start writing data")
+        // Write a message to the database
+        val database = Firebase.database
+//        val myRef = database.reference.child("message").child("position")
+//        myRef.setValue("1")
+        val myRef = database.reference.child("position")
+//        val key = myRef.push().key
+//        val classInfo = mutableMapOf<String, String>("中国" to "China", "英国" to "England")
+        Log.d("test", "courseName: $courseName location: $location ")
+        val courseInfo =mutableMapOf<String, String>().apply {
+            this["courseName"] = courseName
+            this["location"] = location
+        }
+//        myRef.setValue(classInfo)
+        val childUpdates = mutableMapOf<String, Any>(
+            "/$position" to courseInfo,
+        )
+        Log.d("test","$position")
+        myRef.updateChildren(childUpdates)
+
+    }
 
 }
